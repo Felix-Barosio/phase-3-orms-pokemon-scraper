e02143855604ac2414c3223fedbc7f47e8f6c216
Felix <barosiofelix@gmail.com>
2022-10-30 20:22:00 +0300

    Completed Assignment

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,37 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+
+    end
+
+
+    def self.save(name, type, db)
+
+        sql = <<-SQL
+        INSERT INTO Pokemon (name, type) VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+
+    end
+
+    def self.find(id, db)
+
+        sql = <<-SQL
+        SELECT * FROM Pokemon WHERE id = ?
+        SQL
+
+        poke = db.execute(sql, id).flatten
+        self.new(id: poke[0], name: poke[1], type: poke[2], db: db)
+
+    end
+
+
 end
